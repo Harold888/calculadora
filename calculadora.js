f9f1594d08ba39e9472095ea2440c84178440458
Harold Ramirez <haramirez0893@misena.edu.co>
2023-03-31 14:06:44 -0500

    Vfinal

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,6 @@
+
+var botones=["7","8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", ".", "=", "/","borrar"];
+
 function renderizarGUI(){
 
     //Div Calculadora
@@ -7,7 +10,7 @@ function renderizarGUI(){
     //2. Aagregar atributos (opcional)
     divCalculadora.setAttribute("id", "calculadora");
     divCalculadora.setAttribute("class","text-center");
-    divCalculadora.setAttribute("style", "width:50%; margin:0 auto;")
+    divCalculadora.setAttribute("style", "width:50%; margin:20em auto;")
 
     //3. Insertar el elemnto en el DOM
     document.body.appendChild(divCalculadora);
@@ -30,6 +33,58 @@ function renderizarGUI(){
     const divBotones = document.createElement("div");
     divBotones.setAttribute("id", "botones");
     divCalculadora.appendChild(divBotones);
+
+    for(let i = 0; i<botones.length;i++){
+    
+        //Crear filas
+        if(i%4==0){
+            const divFila = document.createElement("div");
+            divFila.setAttribute("class", "row");
+            divBotones.appendChild(divFila);
+        }
+        let boton = document.createElement("button");
+        boton.setAttribute("id", "boton"+botones[i]);
+        boton.setAttribute("class", "btn btn-info col-3 border-white");
+        boton.innerHTML=botones[i];
+        
+            boton.addEventListener("click", function(){
+                if(botones[i] != "borrar" ){    
+                    procesarEvento(boton);
+                }else{
+                    borrarContenido(boton);
+                }
+            })   
+        divBotones.appendChild(boton);
+        
+    }
 }
+
+
+function procesarEvento(boton){
+
+    let miPantalla = document.getElementById("pantalla");
+    if(miPantalla.value=="0" || miPantalla.value=="Operacion no valida"){
+        miPantalla.value="";
+    }
+    if(boton.innerHTML != "=" ){
+    miPantalla.value+=boton.innerHTML;
+    }else{
+        try{
+        //Evaluar la expresion con math.js
+        let resultado = math.evaluate(miPantalla.value);
+        miPantalla.value=resultado;
+        }catch{
+            let error = "Operacion no valida";
+            miPantalla.value=error;
+        }
+    }
+
+}
+
+function borrarContenido(boton){
+    let miPantalla = document.getElementById("pantalla");
+    miPantalla.value="0";
+}
+
 
 renderizarGUI();
